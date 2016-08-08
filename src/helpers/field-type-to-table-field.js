dee8ca09cbfd034bc05bc9139776165447007b3e
Ryan Tablada <ryan.tablada@gmail.com>
2016-08-08 07:52:48 -0500

    Don't create lines for hasMany relations in migration

--- src/helpers/field-type-to-table-field.js
+++ src/helpers/field-type-to-table-field.js
@@ -1,3 +1,17 @@
+const inflect = require('i')();
+
+function relationToTableField(fieldType) {
+  if (fieldType.relation.type === 'belongsTo') {
+    const reference = inflect.tableize(`${fieldType.relation.modelName}`);
+    return `table.integer('${fieldType.relation.foreignKey}').references('${reference}.id');`;
+  }
+
+  return '';
+}
+
 module.exports = function fieldTypeToTableField(fieldType) {
+  if (fieldType.type === 'relation') {
+    return relationToTableField(fieldType);
+  }
   return `table.${fieldType.type}('${fieldType.name}');`;
 };
--- test/unit/field-type-to-table-field.spec.js
+++ test/unit/field-type-to-table-field.spec.js
@@ -36,26 +36,6 @@ describe('fieldTypeToTableField', () => {
         foreignKey: 'instructor_id',
       },
     },
-    belongsToWithModel: {
-      name: 'groups',
-      type: 'relation',
-      raw: 'groups:hasMany:Cohort:instructor_id',
-      relation: {
-        type: 'hasMany',
-        modelName: 'Cohort',
-        foreignKey: 'instructor_id',
-      },
-    },
-    belongsToWithForeignKey: {
-      name: 'author',
-      type: 'relation',
-      raw: 'author:belongsTo:User',
-      relation: {
-        type: 'belongsTo',
-        modelName: 'User',
-        foreignKey: 'user_id',
-      },
-    },
 
     hasMany: {
       name: 'startups',
@@ -64,26 +44,6 @@ describe('fieldTypeToTableField', () => {
       relation: {
         type: 'hasMany',
         modelName: 'Business',
-        foreignKey: 'pokemon_id',
-      },
-    },
-    hasManyWithModel: {
-      name: 'post',
-      type: 'relation',
-      raw: 'post:belongsTo',
-      relation: {
-        type: 'belongsTo',
-        modelName: 'Post',
-        foreignKey: 'post_id',
-      },
-    },
-    hasManyWithForeignKey: {
-      name: 'dogs',
-      type: 'relation',
-      raw: 'dogs:hasMany',
-      relation: {
-        type: 'hasMany',
-        modelName: 'Dog',
         foreignKey: 'pokemon_id',
       },
     },
@@ -98,21 +58,12 @@ describe('fieldTypeToTableField', () => {
       .equal('table.boolean(\'drive\');');
   });
 
-  it('should know how to parse belongsTo relationships with all fields', () => {
+  it('should know how to parse belongsTo relationships', () => {
+    expect(toTableField(fieldTypes.belongsTo)).to
+      .equal("table.integer('instructor_id').references('users.id');");
   });
 
-  it('should know how to parse hasMany relationships with all fields', () => {
-  });
-
-  it('should know how to parse belongsTo relationships with no foreignKey', () => {
-  });
-
-  it('should know how to parse hasMany relationships with no foreignKey', () => {
-  });
-
-  it('should know how to parse belongsTo relationships with no model', () => {
-  });
-
-  it('should know how to parse hasMany relationships with no model', () => {
+  it('should know how to parse hasMany relationships', () => {
+    expect(toTableField(fieldTypes.hasMany)).to.equal('');
   });
 });
