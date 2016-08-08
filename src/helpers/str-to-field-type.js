2c86d24ffcdd596ebca2442e885987924616920d
Ryan Tablada <ryan.tablada@gmail.com>
2016-08-07 21:20:29 -0500

    Basic relations work with hasMany

--- src/helpers/str-to-field-type.js
+++ src/helpers/str-to-field-type.js
@@ -1,9 +1,15 @@
+const relationshipTypes = [
+  'belongsTo',
+  'hasMany',
+  'hasOne',
+];
+
 module.exports = function strToFieldType(str) {
   const [name, typeName, modelName, foreignKey] = str.split(':');
   let relation;
   let type;
 
-  if (typeName === 'belongsTo') {
+  if (relationshipTypes.includes(typeName)) {
     relation = {
       type: typeName,
       modelName,
--- test/unit/str-to-field-type.spec.js
+++ test/unit/str-to-field-type.spec.js
@@ -20,11 +20,14 @@ describe('strToFieldType', () => {
     integer: 'age:integer',
     string: 'name:string',
     boolean: 'drive:boolean',
+
     belongsTo: 'post:belongsTo',
     belongsToWithModel: 'author:belongsTo:User',
     belongsToWithModelAndForeignKey: 'teacher:belongsTo:User:instructor_id',
+
     hasMany: 'dogs:hasMany',
     hasManyWithModel: 'startups:hasMany:Business',
+    hasManyWithModelAndForeignKey: 'groups:hasMany:Cohort:instructor_id',
   };
 
   it('should know how to parse raw data types', () => {
@@ -50,7 +53,7 @@ describe('strToFieldType', () => {
     });
   });
 
-  it('should know how to parse belongsTo relationships', () => {
+  it('should know how to parse belongsTo relationships with all fields', () => {
     expect(strToFieldType(fieldStrings.belongsToWithModelAndForeignKey)).to.deep.equal({
       name: 'teacher',
       type: 'relation',
@@ -58,6 +61,19 @@ describe('strToFieldType', () => {
       relation: {
         type: 'belongsTo',
         modelName: 'User',
+        foreignKey: 'instructor_id',
+      },
+    });
+  });
+
+  it('should know how to parse hasMany relationships with all fields', () => {
+    expect(strToFieldType(fieldStrings.hasManyWithModelAndForeignKey)).to.deep.equal({
+      name: 'groups',
+      type: 'relation',
+      raw: 'groups:hasMany:Cohort:instructor_id',
+      relation: {
+        type: 'hasMany',
+        modelName: 'Cohort',
         foreignKey: 'instructor_id',
       },
     });
